720b940d580248c045ff1d26a544c1d6deb775d6
Benjamin Blarr <b.blarr@gmx.de>
2025-11-20 15:54:51 +0100

    Added images of the loaded pokemon

--- scripts/script.js
+++ scripts/script.js
@@ -1,60 +1,48 @@
-
-
-
 const refContainer = document.getElementById("pokemon-listing");
 
-
 function init() {
-    loadGermanName();
-    // fetchName();
-
-
-
-
-
+  loadPokemonInfos();
+  
 }
 
-async function fetchName(id) {
-    
-    let newName = "";
-        let response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}/`);
-    let data = await response.json();
-   let pokemon = data.names;
-    for (let i = 0; i < pokemon.length; i++) {
+async function fetchName(id, pokemonImage) {
+  let newName = "";
+  let response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon-species/${id}/`
+  );
+  let data = await response.json();
+  let pokemon = data.names;
+  for (let i = 0; i < pokemon.length; i++) {
     if (pokemon[i].language.name === "de") {
-        newName =  pokemon[i].name;
-        console.log(newName);
-       break;
+      newName = pokemon[i].name;
+      console.log(newName);
+      break;
     }
-      }
-    
-   
-    
-       refContainer.innerHTML += `
+  }
+
+  refContainer.innerHTML += `
             <div class="pokemon-entry">
                 <header id="pokemon-entry-header[${id}]">
                    <span id="pokemon-id[${id}]"># ${id}</span>
                    <h3>${newName}</h3>
                 </header>
-                <section id="pokemon-entry-image[${id}]"></section>
+                <section id="pokemon-entry-image[${id}]"><img src="${pokemonImage}" alt="${newName}"></section>
                 <footer id="pokemon-entry-footer[${id}]"></footer>
             </div>
         `;
-    
 }
 
-async function loadGermanName() {
-    for (let id = 1; id <= 30; id++) {
-        let germanName = await fetchName(id);
-        console.log(germanName);
-        
-    }
+async function loadPokemonInfos() {
+  for (let id = 1; id <= 30; id++) {
+    const pokemonImage = await getPokemonImage(id);
+    await fetchName(id, pokemonImage);
+  }
 }
 
+async function getPokemonImage(id) {
+let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+let data = await response.json();
+let pokemonImage = data.sprites.other["official-artwork"].front_default;
+return pokemonImage;
 
-
-
-//   <p>Farbe: ${pokemon[i].colorGroup}</p>
-//                 <p>ID: ${pokemon[i].beanId}</p>
-//                 <p>Bild: ${pokemon[i].imageUrl}</p>
-//                 <hr></hr>
+}
